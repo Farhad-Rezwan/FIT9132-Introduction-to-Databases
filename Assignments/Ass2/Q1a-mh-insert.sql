eb443d815ba49e869b1bb6fbc9c1d53f1987745a
Farhad Ullah Rezwan <frez0003@student.monash.edu>
2019-05-23 14:06:04 +1000

    week 11 lab

--- Assignments/Ass2/Q1a-mh-insert.sql
+++ Assignments/Ass2/Q1a-mh-insert.sql
@@ -31,30 +31,33 @@ insert into patient values (139998,'Razia','Sultana','103 Burwood Highway, Burwo
 
 
 
-insert into admission values (188207, to_date('23-Apr-19', 'dd-Mon-yyyy'), to_date('26-Apr-19', 'dd-Mon-yyyy'), 113067, 1095);
-insert into admission values (185689, to_date('16-Feb-19', 'dd-Mon-yyyy'), to_date('23-Feb-19', 'dd-Mon-yyyy'), 110030, 7890);
---insert into admission values (154043, to_date('13-May-19', 'dd-Mon-yyyy'), to_date('	 , 'dd-Mon-yyyy'), 112090, 1298);
-insert into admission values (150944, to_date('12-Mar-19', 'dd-Mon-yyyy'), to_date('23-Mar-19', 'dd-Mon-yyyy'), 110000, 1064);
-insert into admission values (152711, to_date('13-Feb-19', 'dd-Mon-yyyy'), to_date('22-Feb-19', 'dd-Mon-yyyy'), 139902, 1028);
-insert into admission values (172850, to_date('19-Mar-19', 'dd-Mon-yyyy'), to_date('22-Mar-19', 'dd-Mon-yyyy'), 136789, 1012);
-insert into admission values (185093, to_date('31-Mar-19', 'dd-Mon-yyyy'), to_date('04-Apr-19', 'dd-Mon-yyyy'), 120781, 1084);
---insert into admission values (180385, to_date('12-May-19', 'dd-Mon-yyyy'), to_date('	 , 'dd-Mon-yyyy'), 116290, 1005	);
-insert into admission values (185711, to_date('03-Mar-19', 'dd-Mon-yyyy'), to_date('25-Mar-19', 'dd-Mon-yyyy'), 110030, 1033);
-insert into admission values (155543, to_date('21-Jan-19', 'dd-Mon-yyyy'), to_date('01-Feb-19', 'dd-Mon-yyyy'), 117239, 1056);
-insert into admission values (159389, to_date('23-Mar-19', 'dd-Mon-yyyy'), to_date('04-Apr-19', 'dd-Mon-yyyy'), 111020, 1012);
-insert into admission values (174522, to_date('01-May-19', 'dd-Mon-yyyy'), to_date('07-May-19', 'dd-Mon-yyyy'), 138900, 1095);
-insert into admission values (156404, to_date('21-Mar-19', 'dd-Mon-yyyy'), to_date('23-Mar-19', 'dd-Mon-yyyy'), 141175, 1005);
-insert into admission values (183296, to_date('25-Feb-19', 'dd-Mon-yyyy'), to_date('26-Feb-19', 'dd-Mon-yyyy'), 143765, 1099);
-insert into admission values (186850, to_date('03-Mar-19', 'dd-Mon-yyyy'), to_date('14-Mar-19', 'dd-Mon-yyyy'), 139902, 1060);
-insert into admission values (156571, to_date('24-Feb-19', 'dd-Mon-yyyy'), to_date('18-Mar-19', 'dd-Mon-yyyy'), 139032, 1018);
-insert into admission values (162634, to_date('07-Jan-19', 'dd-Mon-yyyy'), to_date('06-Feb-19', 'dd-Mon-yyyy'), 125678, 2459);
-insert into admission values (184793, to_date('08-May-19', 'dd-Mon-yyyy'), to_date('18-Feb-19', 'dd-Mon-yyyy'), 139032, 1069);
-insert into admission values (174223, to_date('08-Jan-19', 'dd-Mon-yyyy'), to_date('29-Jan-19', 'dd-Mon-yyyy'), 115890, 1005);
-insert into admission values (170140, to_date('06-Mar-19', 'dd-Mon-yyyy'), to_date('18-Mar-19', 'dd-Mon-yyyy'), 143765, 1027);
-insert into admission values (164688, to_date('09-Jan-19', 'dd-Mon-yyyy'), to_date('16-Jan-19', 'dd-Mon-yyyy'), 114067, 1048);
---insert into admission values (166232, to_date('13-May-19', 'dd-Mon-yyyy'), to_date('	 ', 'dd-Mon-yyyy'), 141242, 1061	);
---insert into admission values (180532, to_date('19-Mar-19', 'dd-Mon-yyyy'), to_date('	 ', 'dd-Mon-yyyy'), 148900, 1099	);
-insert into admission values (188637, to_date('16-Mar-19', 'dd-Mon-yyyy'), to_date('21-Mar-19', 'dd-Mon-yyyy'), 125678, 7900);
-insert into admission values (178613, to_date('13-Apr-19', 'dd-Mon-yyyy'), to_date('22-Apr-19', 'dd-Mon-yyyy'), 128342, 1099);
 
+
+insert into admission values (188207, to_date('23-Apr-2019 12:03:53', 'dd-Mon-yyyy hh24:mi:ss'), to_date('26-Apr-2019 10:33:22', 'dd-Mon-yyyy hh24:mi:ss'), 113067, 1095);
+insert into admission values (185689, to_date('16-Feb-2019 11:02:13', 'dd-Mon-yyyy hh24:mi:ss'), to_date('23-Feb-2019 12:56:42', 'dd-Mon-yyyy hh24:mi:ss'), 110030, 7890);
+insert into admission values (154043, to_date('13-May-2019 04:05:59', 'dd-Mon-yyyy hh24:mi:ss'), null, 112090, 1298);
+insert into admission values (150944, to_date('12-Mar-2019 01:03:09', 'dd-Mon-yyyy hh24:mi:ss'), to_date('23-Mar-2019 13:55:44', 'dd-Mon-yyyy hh24:mi:ss'), 110000, 1064);
+insert into admission values (152711, to_date('13-Feb-2019 02:55:34', 'dd-Mon-yyyy hh24:mi:ss'), to_date('22-Feb-2019 00:49:33', 'dd-Mon-yyyy hh24:mi:ss'), 139902, 1028);
+insert into admission values (172850, to_date('19-Mar-2019 14:23:06', 'dd-Mon-yyyy hh24:mi:ss'), to_date('22-Mar-2019 22:33:16', 'dd-Mon-yyyy hh24:mi:ss'), 136789, 1012);
+insert into admission values (185093, to_date('31-Mar-2019 18:11:23', 'dd-Mon-yyyy hh24:mi:ss'), to_date('04-Apr-2019 06:09:26', 'dd-Mon-yyyy hh24:mi:ss'), 120781, 1084);
+insert into admission values (180385, to_date('12-May-2019 19:33:12', 'dd-Mon-yyyy hh24:mi:ss'), null, 116290, 1005	);
+insert into admission values (185711, to_date('03-Mar-2019 22:33:19', 'dd-Mon-yyyy hh24:mi:ss'), to_date('25-Mar-2019 10:12:47', 'dd-Mon-yyyy hh24:mi:ss'), 110030, 1033);
+insert into admission values (155543, to_date('21-Jan-2019 04:55:13', 'dd-Mon-yyyy hh24:mi:ss'), to_date('01-Feb-2019 21:23:35', 'dd-Mon-yyyy hh24:mi:ss'), 117239, 1056);
+insert into admission values (159389, to_date('09-Mar-2019 21:12:12', 'dd-Mon-yyyy hh24:mi:ss'), to_date('04-Apr-2019 23:03:17', 'dd-Mon-yyyy hh24:mi:ss'), 111020, 1012);
+insert into admission values (174522, to_date('01-May-2019 21:55:23', 'dd-Mon-yyyy hh24:mi:ss'), to_date('07-May-2019 03:56:33', 'dd-Mon-yyyy hh24:mi:ss'), 138900, 1095);
+insert into admission values (156404, to_date('21-Mar-2019 18:23:34', 'dd-Mon-yyyy hh24:mi:ss'), to_date('23-Mar-2019 04:12:01', 'dd-Mon-yyyy hh24:mi:ss'), 141175, 1005);
+insert into admission values (183296, to_date('25-Feb-2019 12:56:52', 'dd-Mon-yyyy hh24:mi:ss'), to_date('26-Feb-2019 19:42:56', 'dd-Mon-yyyy hh24:mi:ss'), 143765, 1099);
+insert into admission values (186850, to_date('03-Mar-2019 21:23:53', 'dd-Mon-yyyy hh24:mi:ss'), to_date('14-Mar-2019 02:24:09', 'dd-Mon-yyyy hh24:mi:ss'), 139902, 1060);
+insert into admission values (156571, to_date('24-Feb-2019 12:49:48', 'dd-Mon-yyyy hh24:mi:ss'), to_date('18-Mar-2019 18:01:23', 'dd-Mon-yyyy hh24:mi:ss'), 139032, 1018);
+insert into admission values (162634, to_date('07-Jan-2019 00:28:37', 'dd-Mon-yyyy hh24:mi:ss'), to_date('06-Feb-2019 02:41:51', 'dd-Mon-yyyy hh24:mi:ss'), 125678, 2459);
+insert into admission values (184793, to_date('08-May-2019 09:23:26', 'dd-Mon-yyyy hh24:mi:ss'), to_date('18-Feb-2019 15:01:32', 'dd-Mon-yyyy hh24:mi:ss'), 139032, 1069);
+insert into admission values (174223, to_date('08-Jan-2019 04:11:26', 'dd-Mon-yyyy hh24:mi:ss'), to_date('29-Jan-2019 10:11:49', 'dd-Mon-yyyy hh24:mi:ss'), 115890, 1005);
+insert into admission values (170140, to_date('06-Mar-2019 03:22:56', 'dd-Mon-yyyy hh24:mi:ss'), to_date('18-Mar-2019 05:41:43', 'dd-Mon-yyyy hh24:mi:ss'), 143765, 1027);
+insert into admission values (164688, to_date('09-Jan-2019 13:44:34', 'dd-Mon-yyyy hh24:mi:ss'), to_date('16-Jan-2019 04:26:51', 'dd-Mon-yyyy hh24:mi:ss'), 114067, 1048);
+insert into admission values (166232, to_date('13-May-2019 12:16:12', 'dd-Mon-yyyy hh24:mi:ss'), null, 141242, 1061	);
+insert into admission values (180532, to_date('19-Mar-2019 23:45:53', 'dd-Mon-yyyy hh24:mi:ss'), null, 148900, 1099	);
+insert into admission values (188637, to_date('16-Mar-2019 20:16:31', 'dd-Mon-yyyy hh24:mi:ss'), to_date('21-Mar-2019 00:45:27', 'dd-Mon-yyyy hh24:mi:ss'), 125678, 7900);
+insert into admission values (178613, to_date('13-Apr-2019 19:20:06', 'dd-Mon-yyyy hh24:mi:ss'), to_date('22-Apr-2019 10:42:30', 'dd-Mon-yyyy hh24:mi:ss'), 128342, 1099);
+
+delete from admission;
 delete from patient;
