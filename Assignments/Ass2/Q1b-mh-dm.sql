966b8e087257d2ceafda9b61fd8cb61b07ca9080
Farhad Ullah Rezwan <frez0003@student.monash.edu>
2019-05-28 02:09:09 +1000

    Assignment 2 question 1 (full complete)
    completed question B this time

---
+++ Assignments/Ass2/Q1b-mh-dm.sql
@@ -0,0 +1,103 @@
+/* FIT9132 2019 S1 Assignment 2 Q1-Part B ANSWERS
+   Student Name: Farhad Ullah Rezwan
+    Student ID: 30270111
+
+   Comments to your marker:
+   
+*/
+
+/* (i)*/
+DROP SEQUENCE patient_seq;
+
+DROP SEQUENCE admission_seq;
+
+DROP SEQUENCE adm_prc_seq;
+
+CREATE SEQUENCE patient_seq START WITH 200000 INCREMENT BY 10;
+
+CREATE SEQUENCE admission_seq START WITH 200000 INCREMENT BY 10;
+
+CREATE SEQUENCE adm_prc_seq START WITH 200000 INCREMENT BY 10;
+
+/* (ii)*/
+
+INSERT INTO patient VALUES (
+    patient_seq.NEXTVAL,
+    'Peter',
+    'Xiue',
+    '14 Narrow Lane Caulfield',
+    TO_DATE('01-Oct-1981', 'dd-Mon-yyyy'),
+    '0123456789'
+);
+
+INSERT INTO admission VALUES (
+    admission_seq.NEXTVAL,
+    TO_DATE('16-May-2019 10:00 PM', 'dd-Mon-yyyy hh:mi PM'),
+    NULL,
+    patient_seq.CURRVAL,
+    (
+        SELECT
+            doctor_id
+        FROM
+            doctor
+        WHERE
+            doctor_title = 'Dr'
+            AND doctor_fname = 'Sawyer'
+            AND doctor_lname = 'Haisell'
+    )
+);
+
+COMMIT;
+
+/* (iii)*/
+
+UPDATE doctor_speciality
+SET
+    spec_code = (
+        SELECT
+            spec_code
+        FROM
+            speciality
+        WHERE
+            spec_description = 'Vascular surgery'
+    )
+WHERE
+    doctor_id = (
+        SELECT
+            doctor_id
+        FROM
+            doctor
+        WHERE
+            doctor_title = 'Dr'
+            AND doctor_fname = 'Decca'
+            AND doctor_lname = 'Blankhorn'
+    )
+    AND spec_code = (
+        SELECT
+            spec_code
+        FROM
+            speciality
+        WHERE
+            spec_description = 'Thoracic surgery'
+    );
+
+COMMIT;  
+
+/* (iv)*/
+
+DELETE FROM doctor_speciality
+WHERE
+    spec_code = (
+        SELECT
+            spec_code
+        FROM
+            speciality
+        WHERE
+            spec_description = 'Medical genetics'
+    );
+
+DELETE FROM speciality
+WHERE
+    spec_description = 'Medical genetics';
+
+COMMIT;
