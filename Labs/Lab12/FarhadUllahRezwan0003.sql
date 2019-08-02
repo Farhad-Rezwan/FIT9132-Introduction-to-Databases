92b3d7cdd7720676f5f0fff56c60d05b96d015bb
Farhad Ullah Rezwan <frez0003@student.monash.edu>
2019-08-02 15:26:35 +1000

    does it work

--- Labs/Lab12/FarhadUllahRezwan0003.sql
+++ Labs/Lab12/FarhadUllahRezwan0003.sql
@@ -1,13 +1,22 @@
 SELECT
     u.unitcode,
-    unitname,
-    COUNT(has_prere_of)
+    COUNT(p.has_prereq_of)
 FROM
-    u.unitcode   u
-    LEFT OUTER JOIN uni.prereq   p on u.unitcode group by by unitcode, unitname ;
+    uni.unit     u
+    LEFT OUTER JOIN uni.prereq   p
+    ON u.unitcode = p.unitcode
+GROUP BY
+    u.unitcode
+ORDER BY
+    u.unitcode;
 
-select * from uni.unit;
-select * form uni.pre)req;
+SELECT
+    *
+FROM
+    uni.unit;
+
+SELECT
+    * form uni.pre)req;
 
 --2
 select unitcode, unitname FROM uni.unit
@@ -15,14 +24,49 @@ minus-- can use union  data type should exactly same
 select u.unitcode, u.unitname from uni.unit u join uni.prereq p on u.unitcode = p.unitcode;
 
 --another way of ding
-select unitcode, unitname from uni.unit where unitcode not in (
-select unitcode from uni.prereq)
+select unitcode, unitname from uni.unit where unitcode NOT
+in
 
+( SELECT
+    unitcode
+FROM
+    uni.prereq
+)
+ORDER BY
+    unitcode;
+
+/*3*/
+
+SELECT
+    unitcode,
+    TO_CHAR(ofyear, 'yyyy') AS year,
+    semester,
+    TO_CHAR(nvl(AVG(mark), 0), '900.99') AS average
+FROM
+    uni.enrolment
+GROUP BY
+    unitcode,
+    ofyear,
+    semester
+ORDER BY
+    unitcode;
+
+--2
+select unitcode, unitname from uni.unit
+minus
+select u.unitcode, unitname from uni.unit u join uni.prereq p on u.unitcode = p.unitcode;
+
+----outer join
+
+select u.unitcode, u.unitname 
+from uni.unit u 
+left outer JOIN uni.prereq p
+ON u.unitcode = p.unitcode 
+group BY u.unitcode,
+         u.unitname
+HAVING
+    COUNT(has_prereq_of) = 0 
 order by unitcode;
 
---3
-select unitcode, to_char(ofyear, 'yyyy') as year , semester, to_char(nvl(avg(mark),0),'900.99')
-as average from uni.enrolment
-group by unitcode, ofyear, semester
-order by unitcode;
-
+-- subquary
+select unitcode, unitname from uni.unit where unitcode not in (select unitcode from uni.prereq) order by unitcode;
